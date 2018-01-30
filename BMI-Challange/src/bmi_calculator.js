d3c42d0af5c3c0ba6ac26bacc31970c4b9335aea
bollen <david.boll@me.com>
2018-01-30 15:04:38 +0100

    Addde grunt / coveralls

--- BMI-Challange/src/bmi_calculator.js
+++ BMI-Challange/src/bmi_calculator.js
@@ -28,6 +28,7 @@ BMICalculator.prototype.metric_bmi = function(obj) {
   var weight = obj.weight;
   var height = obj.height;
   var selection = obj.selection;
+
   if (weight > 0 && height > 0) {
     if (selection == "metric") {
       var finalBmi = weight / (height / 100 * height / 100);
